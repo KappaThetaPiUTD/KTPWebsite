48537714b550bd59e1ccd2538927a52b9805c448
pakkuu <ethan.pvarghese@gmail.com>
2024-04-19 10:03:40 -0500

    created contact-us page

---
+++ app/contact-us/page.jsx
@@ -0,0 +1,15 @@
+import Inputs from "../components-pakku/Inputs";
+import SubmitButton from "../components-pakku/SubmitButton"
+
+const ContactUs = () => {
+  return (
+    <div className="flex flex-col w-screen h-[2000px] bg-[#0F0F0F] items-center pt-[52px]">
+      <div className="text-secondary font-poppins text-[86px] font-bold mb-[0px]">Contact Us</div>
+      <div className="text-[#FFFFFF] text-[45px]">Have a question or concern? Hit us up.</div>
+      <Inputs/>
+      <SubmitButton/>
+    </div>
+  );
+};
+
+export default ContactUs;
