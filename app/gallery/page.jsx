feb072d5b9ee1d67bb04d6ff1a39111f5be75aa0
kairavipandya <104184341+kairavipandya@users.noreply.github.com>
2024-05-04 16:00:05 -0500

    done! yippee

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -6,9 +6,8 @@ const Gallery = () => {
   const images = [
     { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850376/Austin%20Trip/100_1531_yphg3y.jpg', alt: 'Image 1' },
     { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850373/Austin%20Trip/100_1532_kyjzgm.jpg', alt: 'Image 2' },
-    { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850372/Austin%20Trip/100_1535_kyvpmo.jpg', alt: 'Image 3' },
-    { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850372/Austin%20Trip/100_1537_ji9dto.jpg', alt: 'Image 4' },
     { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850372/Austin%20Trip/100_1536_zdnmmh.jpg', alt: 'Image 5' },
+    { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850372/Austin%20Trip/100_1537_ji9dto.jpg', alt: 'Image 4' },
     { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850371/Austin%20Trip/100_1541_taziyw.jpg', alt: 'Image 6' },
     { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850371/Austin%20Trip/100_1538_icfqfn.jpg', alt: 'Image 7' },
     { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850371/Austin%20Trip/100_1543_eo1niy.jpg', alt: 'Image 8' },
@@ -17,6 +16,13 @@ const Gallery = () => {
     { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850366/Austin%20Trip/100_1549_hhphkv.jpg', alt: 'Image 12' },
     { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850366/Austin%20Trip/100_1555_zr1jtr.jpg', alt: 'Image 13' },
     { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714850362/Austin%20Trip/100_1560_ayi2vm.jpg', alt: 'Image 14' },
+    { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714854893/Austin%20Trip/leetcode_weekly_session_images_kqkd9s.jpg', alt: 'leetcode'},
+    { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714854890/Austin%20Trip/leetcode_tariq_teaching_ozyzaj.jpg', alt: 'tariq teaching'},
+    { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714855358/first_chapter_meeting_e3yoyf.jpg', alt: 'new gen first chapter meeting'},
+    { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714855358/social_legdri.jpg', alt: 'social valentines day 2024'},
+    { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714855358/happy_ktp_hcb7r6.jpg', alt: 'happy ktp 2024'},
+    { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714855358/valentines_day_uno_z3tgnz.jpg', alt: 'social valentines day 2024'},
+    { src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1714855359/ktp_valetines_day_social_affiq_pocker_jmm8u1.jpg', alt: 'social valentines day 2024'}
   ];
 
   return (
@@ -24,17 +30,10 @@ const Gallery = () => {
       <div className="text-secondary text-header1 font-bold font-poppins flex justify-center">
         Gallery
       </div>
+      <div className="text-gray text-center font-georgia text-header4 mt-2">
+        We would like to thank our photographers Tariq Mahamid and Manasa Paruchuri!
+      </div>
       <div className="grid grid-cols-3 gap-4 p-4">
-        <div className="relative overflow-hidden rounded-lg flex justify-center items-center">
-          <div className="text-white bg-gray-800 p-4 rounded-lg border border-white">
-            <p className="text-center">Our Photographers</p>
-          </div>
-        </div>
-                <div className="relative overflow-hidden rounded-lg flex justify-center items-center">
-          <div className="text-white bg-gray-800 p-4 rounded-lg border border-white">
-            <p className="text-center">KTP Retreat</p>
-          </div>
-        </div>
         {images.map((img, index) => (
           <div key={index} className="relative overflow-hidden rounded-lg">
             <div className="group relative">
