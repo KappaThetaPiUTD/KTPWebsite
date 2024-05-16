a239c2ee95b293eb4bedfe56a30ccfc8c77d89a7
kairavipandya <104184341+kairavipandya@users.noreply.github.com>
2024-05-16 14:01:54 -0500

    added linkedin

--- app/brother/page.jsx
+++ app/brother/page.jsx
@@ -1,39 +1,39 @@
 // page.jsx
 import Image from 'next/image';
 import React from 'react';
+import { FaLinkedin } from 'react-icons/fa'; // LinkedIn icon
 
 const executiveBoardMembers = [
-  { name: 'Ethan Lobo', position: 'President', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg' },
-  { name: 'Tariq Mahamid', position: 'VP of Technology', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg' },
-  { name: 'Afsar Arif', position: 'VP of Internal Affairs', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg' },
-  { name: 'Avani Melhotra', position: 'VP of Engagement', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg' },
-  { name: 'Laiba Piracha', position: 'VP of Membership', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg' },
-  { name: 'Rushil Patel', position: 'VP of Professional Development', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg' },
-  { name: 'Arya Thombre', position: 'VP of Communications', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg' }
+  { name: 'Ethan Lobo', position: 'President', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/ethanlobo' },
+  { name: 'Tariq Mahamid', position: 'VP of Technology', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/tariqmahamid' },
+  { name: 'Afsar Arif', position: 'VP of Internal Affairs', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/afsararif' },
+  { name: 'Avani Melhotra', position: 'VP of Engagement', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/avanimelhotra' },
+  { name: 'Laiba Piracha', position: 'VP of Membership', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/laibapiracha' },
+  { name: 'Rushil Patel', position: 'VP of Professional Development', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/rushilpatel' },
+  { name: 'Arya Thombre', position: 'VP of Communications', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/aryathombre' }
 ];
 
 const activeMembers = [
-  'Mansi Cherukupally',
-  'Ethan Varghese',
-  'Sanjana Neelee',
-  'Manasa Paruchuri',
-  'Ibrahim Khan',
-  'Kairavi Pandya',
-  'Sumedha Suseendrababu',
-  'Gloria Tu',
-  'Reuben John',
-  'Annan Ahmed',
-  'Humsini Revuru',
-  'Aashna Pathi',
-  'Wildan Susanto',
-  'Monish Ravishankar',
-  'Shalom Michael',
-  'Tariq Mahamid',
-  'Viraaj Veeramachaneni',
-  'Yeshas Nath',
-  'Joshua Solomon',
-  'Arjun Prabhune'
-].map(name => ({ name, src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg' }));
+  { name: 'Mansi Cherukupally', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/mansicherukupally' },
+  { name: 'Ethan Varghese', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715885018/Brother%20Page/Actives/KTP_LOGO_aj24pt.png', linkedin: 'https://www.linkedin.com/in/ethanvarghese' },
+  { name: 'Sanjana Neelee', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/sanjananeelee' },
+  { name: 'Manasa Paruchuri', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715885018/Brother%20Page/Actives/KTP_LOGO_aj24pt.png', linkedin: 'https://www.linkedin.com/in/manasaparuchuri' },
+  { name: 'Ibrahim Khan', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/ibrahimkhan' },
+  { name: 'Kairavi Pandya', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715884826/Brother%20Page/Actives/Kairavi_Pandya_Headshot_vearh8.jpg', linkedin: 'https://www.linkedin.com/in/kairavipandya' },
+  { name: 'Sumedha Suseendrababu', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/sumedhasuseendrababu' },
+  { name: 'Gloria Tu', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715885018/Brother%20Page/Actives/KTP_LOGO_aj24pt.png', linkedin: 'https://www.linkedin.com/in/gloriatu' },
+  { name: 'Reuben John', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715885018/Brother%20Page/Actives/KTP_LOGO_aj24pt.png', linkedin: 'https://www.linkedin.com/in/reubenjohn' },
+  { name: 'Annan Ahmed', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715885018/Brother%20Page/Actives/KTP_LOGO_aj24pt.png', linkedin: 'https://www.linkedin.com/in/annanahmed' },
+  { name: 'Humsini Revuru', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/humsinirevuru' },
+  { name: 'Aashna Pathi', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715885018/Brother%20Page/Actives/KTP_LOGO_aj24pt.png', linkedin: 'https://www.linkedin.com/in/aashnapathi' },
+  { name: 'Wildan Susanto', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/wildansusanto' },
+  { name: 'Monish Ravishankar', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/monishravishankar' },
+  { name: 'Shalom Michael', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715885018/Brother%20Page/Actives/KTP_LOGO_aj24pt.png', linkedin: 'https://www.linkedin.com/in/shalommichael' },
+  { name: 'Viraaj Veeramachaneni', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/viraajveeramachaneni' },
+  { name: 'Yeshas Nath', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/yeshasnath' },
+  { name: 'Joshua Solomon', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715885018/Brother%20Page/Actives/KTP_LOGO_aj24pt.png', linkedin: 'https://www.linkedin.com/in/joshuasolomon' },
+  { name: 'Arjun Prabhune', src: 'https://res.cloudinary.com/dha44tosd/image/upload/v1715882072/Brother%20Page/Executive%20Board/ethan_headshot_gnmqtg.jpg', linkedin: 'https://www.linkedin.com/in/arjunprabhune' }
+].sort((a, b) => a.name.localeCompare(b.name));
 
 const alphaClassMembers = [
   'Aditya Sajeev',
@@ -76,20 +76,29 @@ const Brother = () => {
   const betaColumns = splitIntoColumns(betaClassMembers, 3);
 
   return (
-    <div className="w-screen bg-[#0F0F0F] text-white">
+    <div className="w-screen h-full bg-[#0F0F0F] text-white">
       <div className="text-secondary text-header1 font-bold font-poppins flex justify-center mt-8">
         Executive Board
       </div>
       <div className="flex flex-wrap justify-center mt-8">
         {executiveBoardMembers.map((member, index) => (
-          <div key={index} className="m-4 text-center">
+          <div key={index} className="m-4 text-center relative group">
             <div className="relative w-32 h-32 bg-gray-400 rounded-lg overflow-hidden">
               <Image
                 src={member.src}
                 alt={`${member.name} - ${member.position}`}
                 layout="fill"
                 objectFit="cover"
+                className="transition-all duration-300 group-hover:brightness-50"
               />
+              <a
+                href={member.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+              >
+                <FaLinkedin className="text-white text-4xl" />
+              </a>
             </div>
             <div className="mt-2">{member.name}</div>
             <div className="text-sm text-gray-400">{member.position}</div>
@@ -102,14 +111,23 @@ const Brother = () => {
       </div>
       <div className="flex flex-wrap justify-center mt-8">
         {activeMembers.map((member, index) => (
-          <div key={index} className="m-4 text-center">
+          <div key={index} className="m-4 text-center relative group">
             <div className="relative w-32 h-32 bg-gray-400 rounded-lg overflow-hidden">
               <Image
                 src={member.src}
                 alt={`${member.name}`}
                 layout="fill"
                 objectFit="cover"
+                className="transition-all duration-300 group-hover:brightness-50"
               />
+              <a
+                href={member.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="absolute inset-0 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+              >
+                <FaLinkedin className="text-white text-4xl" />
+              </a>
             </div>
             <div className="mt-2">{member.name}</div>
           </div>
