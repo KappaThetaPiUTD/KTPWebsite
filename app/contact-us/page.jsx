db34737e59268e43b3e43936d69eb8eda2e46dfd
pakkuu <ethan.pvarghese@gmail.com>
2024-06-06 11:44:33 -0500

    edited mobile styling

--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -39,15 +39,6 @@ const ContactUs = () => {
     message: "",
   });
 
-  // Reducer that holds first name, last name, email, phone number, and message
-  const [state, dispatch] = useReducer(reducer, {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phoneNumber: "",
-    message: "",
-  });
-
   return (
     <>
       <title>Kappa Theta Pi UTD - Contact Us</title>
