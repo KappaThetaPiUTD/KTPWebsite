15ee6b97e56b2da0504cb5a7dfd1cf5ed4b89761
kairavipandya <104184341+kairavipandya@users.noreply.github.com>
2024-06-26 09:34:38 -0500

    updated nav to be centered

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -31,29 +31,28 @@ const Navbar = () => {
 
   return (
     <div className="fixed bg-[#0F0F0F] w-full z-50">
-      <div className="bg-[#0F0F0F] flex justify-around items-center h-24 max-w-[825px] mx-auto px-4 text-white ">
+      <div className="bg-[#0F0F0F] flex justify-between items-center h-24 max-w-[1200px] mx-auto px-4 text-white">
+
         {/* Desktop Navigation - Hidden on Mobile */}
-        <div className="flex-grow flex justify-start transition duration-500">
-          <ul className="hidden md:flex">
-            {navItems.slice(1, 4).map((item) => (
-              <li
-                key={item.id}
-                className="relative p-4 m-2 cursor-pointer group"
-                onMouseEnter={() => setIsHovered(item.id)}
-                onMouseLeave={() => setIsHovered(null)}
-              >
-                <Link href={item.path}>{item.text}</Link>
-                <div
-                  className={`absolute bottom-0 left-0 right-0 h-0.5 bg-[#00df9a] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-700 origin-center`}
-                  style={{ width: isHovered === item.id ? "100%" : "0" }}
-                />
-              </li>
-            ))}
-          </ul>
+        <div className="hidden md:flex space-x-6">
+          {navItems.slice(1, 4).map((item) => (
+            <li
+              key={item.id}
+              className="list-none relative p-4 cursor-pointer group"
+              onMouseEnter={() => setIsHovered(item.id)}
+              onMouseLeave={() => setIsHovered(null)}
+            >
+              <Link href={item.path}>{item.text}</Link>
+              <div
+                className={`absolute bottom-0 left-0 right-0 h-0.5 bg-[#00df9a] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-700 origin-center`}
+                style={{ width: isHovered === item.id ? "100%" : "0" }}
+              />
+            </li>
+          ))}
         </div>
 
-        {/* Logo - Centered on Desktop, Left on Mobile */}
-        <div className="mx-auto fixed left-0 pl-7 right-0 text-center w-[100px]">
+        {/* Logo - Centered */}
+        <div className="absolute left-1/2 transform -translate-x-1/2 text-center">
           <Link href={navItems[0].path}>
             <h1 className="text-5xl font-bold font-[Greek] text-[#00df9a]">
               KΘΠ
@@ -62,27 +61,25 @@ const Navbar = () => {
         </div>
 
         {/* Right Navigation - Hidden on Mobile */}
-        <div className="flex-grow flex justify-end transition duration-500">
-          <ul className="hidden md:flex">
-            {navItems.slice(4).map((item) => (
-              <li
-                key={item.id}
-                className="relative p-4 m-2 cursor-pointer group"
-                onMouseEnter={() => setIsHovered(item.id)}
-                onMouseLeave={() => setIsHovered(null)}
-              >
-                <Link href={item.path}>{item.text}</Link>
-                <div
-                  className={`absolute bottom-0 left-0 right-0 h-0.5 bg-[#00df9a] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-700 origin-center`}
-                  style={{ width: isHovered === item.id ? "100%" : "0" }}
-                />
-              </li>
-            ))}
-          </ul>
+        <div className="hidden md:flex space-x-6">
+          {navItems.slice(4).map((item) => (
+            <li
+              key={item.id}
+              className="list-none relative p-4 cursor-pointer group"
+              onMouseEnter={() => setIsHovered(item.id)}
+              onMouseLeave={() => setIsHovered(null)}
+            >
+              <Link href={item.path}>{item.text}</Link>
+              <div
+                className={`absolute bottom-0 left-0 right-0 h-0.5 bg-[#00df9a] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-700 origin-center`}
+                style={{ width: isHovered === item.id ? "100%" : "0" }}
+              />
+            </li>
+          ))}
         </div>
 
         {/* Mobile Navigation Icon */}
-        <div onClick={handleNav} className="md:hidden z-50">
+        <div onClick={handleNav} className="md:hidden z-50 absolute right-4">
           {nav ? (
             <AiOutlineClose size={20} className="text-white" />
           ) : (
@@ -90,9 +87,10 @@ const Navbar = () => {
           )}
         </div>
 
+
         {/* Mobile Navigation Menu */}
         <ul
-          className={`absolute md:hidden h-screen w-full top-24 bottom-0 left-0 ease-in-out transition-transform transform duration-700 bg-[#0F0F0F] z-50 ${
+          className={`absolute md:hidden h-screen w-full top-24 bottom-0 right-0 ease-in-out transition-transform transform duration-700 bg-[#0F0F0F] z-50 ${
             nav ? "translate-x-0" : "translate-x-full"
           }`}
         >
