ee958f2452ec71137f0d14b311a9eb4691b4d227
kairavipandya <104184341+kairavipandya@users.noreply.github.com>
2024-06-26 09:51:28 -0500

    mobile responsiveness for footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,31 +1,27 @@
-import { CiLinkedin, CiInstagram, CiLink } from "react-icons/ci";
+import { CiLinkedin, CiInstagram, CiMail } from "react-icons/ci";
 import MailIcon from "./mail-icon-footer";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col sm:flex-row justify-between items-center bottom-0 w-full z-10 bg-[#0f0f0f] text-[#A9A9A9] p-5">
-      <div className="flex flex-col sm:flex-row items-center w-auto">
+    <footer className="flex flex-col sm:flex-row justify-between items-center bottom-0 w-full z-10 bg-[#0f0f0f] text-[#ffffff] p-5">
+      <div className="flex flex-col sm:flex-row items-center w-full sm:w-auto">
         <div className="flex items-center">
-          <MailIcon />
-          <div className="flex flex-col sm:flex-row items-center ml-5">
-            <a href="https://www.linkedin.com/company/ktputd" target="_blank">
-              <CiLinkedin className="mr-[2px]" size={50} />
+          <MailIcon className="hidden sm:block w-10 h-10 text-[#ffffff]" />
+          <div className="flex flex-col sm:flex-row items-center ml-0 sm:ml-5 mt-5 sm:mt-0">
+            <a href="https://www.linkedin.com/company/ktputd" target="_blank" rel="noopener noreferrer">
+              <CiLinkedin className="mr-0 sm:mr-[2px] text-[#ffffff]" size={50} />
             </a>
-            <a
-              href="https://www.instagram.com/utdktp/"
-              target="_blank"
-              className="sm:ml-5"
-            >
-              <CiInstagram className="mr-[2px]" size={50} />
+            <a href="https://www.instagram.com/utdktp/" target="_blank" rel="noopener noreferrer" className="sm:ml-5">
+              <CiInstagram className="mr-0 sm:mr-[2px] text-[#ffffff]" size={50} />
             </a>
           </div>
         </div>
       </div>
-      <div className="text-right mt-5 sm:mt-0">
-        <div className="text-[25px] font-extralight mb-[1px] font-poppins">
+      <div className="text-center sm:text-right mt-5 sm:mt-0">
+        <div className="text-[20px] sm:text-[25px] font-extralight mb-1 font-poppins">
           Kappa Theta Pi - Mu Chapter
         </div>
-        <div className="text-[0.8rem] font-extralight mb-[1px] font-poppins">
+        <div className="text-[0.8rem] font-extralight mb-1 font-poppins">
           Managed by Membership Committee
         </div>
       </div>
