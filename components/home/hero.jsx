fc742f91a6f4e27a46c9fd11a4e200d8100217d8
kairavipandya <104184341+kairavipandya@users.noreply.github.com>
2024-06-26 11:08:09 -0500

    home page responsive

--- components/home/hero.jsx
+++ components/home/hero.jsx
@@ -19,11 +19,11 @@ const Hero = () => {
         unoptimized
       />
       <div className="absolute inset-0 bg-black opacity-40"></div>
-      <div className="flex flex-col items-center gap-y-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap font-georgia text-4xl md:text-6xl lg:text-7xl xl:text-8xl tracking-widest">
+      <div className="flex flex-col items-center gap-y-4 sm:gap-y-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap font-georgia text-3xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl tracking-widest">
         <div>Kappa Theta Pi</div>
-        <div className="text-xl md:text-2xl xl:text-3xl">Mu Chapter</div>
+        <div className="text-lg sm:text-xl md:text-2xl xl:text-3xl">Mu Chapter</div>
       </div>
-      <div className="absolute font-georgia text-xl md:text-3xl lg:text-4xl xl:text-5xl text-[#9B9B9B] left-1/2 transform -translate-x-1/2 bottom-16 whitespace-nowrap">
+      <div className="absolute font-georgia text-lg sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl text-[#9B9B9B] left-1/2 transform -translate-x-1/2 bottom-12 sm:bottom-16 whitespace-nowrap">
         The University of Texas at Dallas
       </div>
       <div
@@ -32,7 +32,7 @@ const Hero = () => {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-10 w-10 text-[#9B9B9B]"
+          className="h-8 w-8 sm:h-10 sm:w-10 text-[#9B9B9B]"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
--- components/home/who-we-is.jsx
+++ components/home/who-we-is.jsx
@@ -7,7 +7,7 @@ const WhoWeIs = () => {
       id="who-we-are"
       className="flex flex-col items-center justify-around w-screen h-[75vh] p-8 bg-[#3C3C3C]"
     >
-      <div className="font-poppins font-bold text-header1 text-[#188C5D]">
+      <div className="font-poppins font-bold text-header1 text-[#188C5D] text-center">
         Who are we?
       </div>
       <div className="relative w-full max-w-4xl h-[50vh] md:h-[60vh] lg:h-[70vh]">
@@ -19,7 +19,7 @@ const WhoWeIs = () => {
           className="absolute"
         />
         <div className="absolute inset-0 flex items-center justify-center z-10">
-          <div className="text-white px-6 md:px-12 lg:px-16 xl:px-20 max-w-[70%] font-poppins text-center leading-relaxed text-sm md:text-base lg:text-lg xl:text-xl">
+          <div className="text-white px-4 sm:px-6 md:px-12 lg:px-16 xl:px-20 max-w-[70%] font-poppins text-center leading-relaxed text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
             Welcome to Kappa Theta Pi Mu Chapter at UT Dallas! We&apos;re all
             about fostering tech skills, creativity, and community. Join us for
             workshops, activities, and vibrant social events to grow
