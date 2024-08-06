f7e98801c982a82d34ec2d4a37809a948cb4d49f
AfsarArif <Mohamedafsar.arif@gmail.com>
2024-08-05 20:41:16 -0500

    mobile view form update

--- app/recruitment/form.jsx
+++ app/recruitment/form.jsx
@@ -8,7 +8,7 @@ const FormPage = () => {
         <div className="text-[#9B1E2E] text-[32px] md:text-header1 font-bold font-poppins text-center">
           Recruitment
         </div>
-        <div className="h-[705px] w-[300px] md:w-[600px] bg-white p-8 rounded-md">
+        <div className="h-[800px] md:h-[705px] w-[300px] md:w-[600px] bg-white p-8 rounded-md">
         <iframe data-tally-src="https://tally.so/embed/npGyb1?alignLeft=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="400" frameborder="0" marginheight="0" marginwidth="0" title="KTP Interest Form"></iframe>
             <Script
                 src="https://tally.so/widgets/embed.js"
