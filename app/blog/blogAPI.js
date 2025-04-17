d6f458d93114d09997afe60a0c8a7650ce3e1975
Mekha Mathew <mam220020@utdallas.edu>
2025-04-17 10:35:26 -0500

    Update blogAPI.js

--- app/blog/blogAPI.js
+++ app/blog/blogAPI.js
@@ -1,5 +1,10 @@
 import { ascending } from "d3-array";
-import { supabase } from "./blogSupabaseClient";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseURL, supabaseAnonKey);
 
 export const fetchPosts = async () => {
     const { data, error } = await supabase
@@ -18,7 +23,7 @@ export const fetchPosts = async () => {
 export const addPost = async(title, content, author) => {
     const { data, error } = await supabase
     .from("blog_posts")
-    .insert([{ title, content, author }]);
+    .insert([{ title, content, author, approved: false }]);
 
     if(error){
         console.error("Error adding post:", error);
@@ -27,3 +32,17 @@ export const addPost = async(title, content, author) => {
 
     return data;
 };
+
+export const approvePost = async (postID) => {
+    const { data, error } = await supabase
+        .from("blog_posts")
+        .update({ approved: true })
+        .eq("id", postID);
+
+        if(error){
+            console.error("Error approving post: ", error);
+            return null;
+        }
+
+        return data;
+}
