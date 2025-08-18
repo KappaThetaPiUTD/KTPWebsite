806cecd756665bc8fce810fdfb4264d5fe42e5c7
Arnav Jain <arnavjainpro@gmail.com>
2025-08-18 01:14:10 -0500

    added work in progress for check in and attendance page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -311,7 +311,7 @@ export default function Dashboard() {
           {/* Check-in card */}
           <div className="bg-white border border-gray-200 p-6 rounded-xl shadow-sm">
             <h3 className="text-base font-semibold mb-3">
-              Check-In for Chapter
+              Check-In for Chapter -- Work in Progress
             </h3>
             <div className="flex justify-between items-center bg-white p-4 border rounded-lg mb-4">
               <div className="rounded-full bg-primary text-white px-4 py-2 text-xs font-semibold truncate max-w-[10rem] text-center">
